6f9465f990c523e405de441bb8b9355aeb9eae19
haifaarfaoui <haifa.arfaoui@gmail.com>
2024-05-06 15:32:35 +0200

    Initial Commit

---
+++ CaptureMoment/CaptureMomentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureMomentApp.swift
+//  CaptureMoment
+//
+//  Created by Haifa Arfaoui on 06.05.24.
+//
+
+import SwiftUI
+
+@main
+struct CaptureMomentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureMoment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptureMoment
+//
+//  Created by Haifa Arfaoui on 06.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
